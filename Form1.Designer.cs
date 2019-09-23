e0813e4b016bd3ed11de1b2f5f1e5893b863790a
Umut Gültekin <43123688+umutgultekn@users.noreply.github.com>
2019-09-23 18:49:07 +0300

    OptimalMismatch

---
+++ Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace OptimalMismatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Location = new System.Drawing.Point(37, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(717, 527);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(984, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(33, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kaynak";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(788, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Pattern :";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(984, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Arama";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(788, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bulunan İndeksler :";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(984, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 132);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(788, 510);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Eşleşme Sayısı : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(984, 508);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(788, 615);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Süre :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(958, 542);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(984, 183);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 38);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(597, 611);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 31);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Dosya Seç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(981, 615);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 20);
+            this.label7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(33, 617);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(116, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Dosya Yolu :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 617);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(422, 22);
+            this.textBox3.TabIndex = 17;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(792, 125);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.TabIndex = 18;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1320, 714);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptimalMismatch
+{
+    public partial class Form1 : Form
+    {
+        List<string> listFiles = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //string Sourcee, Patternn;
+        OptimalMismatch.Result s;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            { 
+            if (listBox1.Items.Count == 0)
+            {
+
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+
+                OptimalMismatch.InitOptimalSearch(textBox1.Text, richTextBox1.Text);
+                OptimalMismatch.Result result = OptimalMismatch.FindAll();
+                ArrayList arrayList = new ArrayList(result.Indexes);
+                foreach (var index in arrayList)
+                {
+                    listBox1.Items.Add(index);
+                }
+
+                int i = 0;
+                i = listBox1.Items.Count;
+                textBox2.Text = i.ToString();
+
+                sw.Stop();
+                label7.Text = sw.Elapsed.ToString() + " miliseconds";
+            }
+
+            else
+            {
+                listBox1.Items.Clear();
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+
+                OptimalMismatch.InitOptimalSearch(textBox1.Text, richTextBox1.Text);
+                OptimalMismatch.Result result = OptimalMismatch.FindAll();
+                ArrayList arrayList = new ArrayList(result.Indexes);
+                foreach (var index in arrayList)
+                {
+                    listBox1.Items.Add(index);
+                }
+
+                int i = 0;
+                i = listBox1.Items.Count;
+                textBox2.Text = i.ToString();
+
+                sw.Stop();
+                label7.Text = sw.Elapsed.ToString() + " miliseconds";
+            }
+        }
+            else
+            {
+                if (listBox1.Items.Count == 0)
+                {
+
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+
+
+                    OptimalMismatch.InitOptimalSearch(textBox1.Text, richTextBox2.Text);
+                    OptimalMismatch.Result result = OptimalMismatch.FindAll();
+                    ArrayList arrayList = new ArrayList(result.Indexes);
+                    foreach (var index in arrayList)
+                    {
+                        listBox1.Items.Add(index);
+                    }
+
+                    int i = 0;
+                    i = listBox1.Items.Count;
+                    textBox2.Text = i.ToString();
+
+                    sw.Stop();
+                    label7.Text = sw.Elapsed.ToString() + " miliseconds";
+                }
+
+                else
+                {
+                    listBox1.Items.Clear();
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+
+
+                    OptimalMismatch.InitOptimalSearch(textBox1.Text, richTextBox2.Text);
+                    OptimalMismatch.Result result = OptimalMismatch.FindAll();
+                    ArrayList arrayList = new ArrayList(result.Indexes);
+                    foreach (var index in arrayList)
+                    {
+                        listBox1.Items.Add(index);
+                    }
+
+                    int i = 0;
+                    i = listBox1.Items.Count;
+                    textBox2.Text = i.ToString();
+
+                    sw.Stop();
+                    label7.Text = sw.Elapsed.ToString() + " miliseconds";
+                }
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Sourcee = richTextBox1.Text;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            listBox1.Items.Clear();
+            textBox1.Clear();
+            textBox2.Clear();
+            label6.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            Stream myStream;
+            OpenFileDialog openFD = new OpenFileDialog();
+
+            if (openFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                openFD.Filter = "txt (*.txt)|*.txt|html (*.html)|*.html|Hepsi (*.*)|*.*";
+                if ((myStream = openFD.OpenFile()) != null)
+                {
+                    textBox3.Text = openFD.FileName;
+                    richTextBox2.Text = File.ReadAllText(openFD.FileName);
+                    //        //richTextBox1.LoadFile(openFD.FileName,RichTextBoxStreamType.PlainText);
+                    //        string fileName = openFD.FileName;
+                    //        string fileText = File.ReadAllText(fileName);
+                    //        //richTextBox1.Text = fileText;
+                }
+            }
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Patternn = textBox1.Text;
+        }
+    }
+
+    class OptimalMismatch
+    {
+        private static char[] _x, _y;
+        private static int _m, _n;
+        private static int[] _adaptedGs, _qsBc, _frequency;
+        private static Pattern[] _pattern;
+
+        private static void OrderPattern(char[] x, Pattern[] pat, int[] freq)
+        {
+            for (int i = 0; i < x.Length; ++i)
+            {
+                Pattern ptrn = new Pattern();
+                ptrn.Location = i;
+                ptrn.Character = x[i];
+                pat[i] = ptrn;
+            }
+
+            QuickSortPattern(pat, 0, x.Length - 1, freq);
+        }
+
+        private static void QuickSortPattern(Pattern[] pat, int low, int n, int[] freq)
+        {
+            int lo = low;
+            int hi = n;
+
+            if (lo >= n) return;
+
+            Pattern mid = pat[(lo + hi) / 2];
+
+            while (lo < hi)
+            {
+                while (lo < hi && OptimalPatternCompare(pat[lo], mid, freq) < 0) lo++;
+                while (lo < hi && OptimalPatternCompare(pat[hi], mid, freq) > 0) hi--;
+
+                if (lo < hi)
+                {
+                    Pattern temp = pat[lo];
+                    pat[lo] = pat[hi];
+                    pat[hi] = temp;
+                }
+            }
+
+            if (hi < lo)
+            {
+                int temp = hi;
+                hi = lo;
+                lo = temp;
+            }
+
+            QuickSortPattern(pat, low, lo, freq);
+            QuickSortPattern(pat, lo == low ? lo + 1 : lo, n, freq);
+        }
+
+        private static int OptimalPatternCompare(Pattern pat1, Pattern pat2, int[] freq)
+        {
+            int fx = freq[pat1.Character] - freq[pat2.Character];
+            return (fx != 0 ? (fx > 0 ? 1 : -1) : (pat2.Location - pat1.Location));
+        }
+
+        private static int MatchShift(char[] x, int ploc, int lShift, Pattern[] pat)
+        {
+            int i, j;
+            for (; lShift < x.Length; ++lShift)
+            {
+                i = ploc;
+                while (--i >= 0)
+                {
+                    if ((j = (pat[i].Location - lShift)) < 0) continue;
+                    if (pat[i].Character != x[j]) break;
+                    
+                }
+                if (i < 0) break;
+            }
+
+            return (lShift);
+        }
+
+        private static void PreAdaptedGs(char[] x, int[] adaptedGs, Pattern[] pat)
+        {
+            int lShift, i, pLoc;
+            adaptedGs[0] = lShift = 1;
+
+            for (pLoc = 1; pLoc <= x.Length; ++pLoc)
+            {
+                lShift = MatchShift(x, pLoc, lShift, pat);
+                adaptedGs[pLoc] = lShift;
+            }
+
+            for (pLoc = 0; pLoc < x.Length; ++pLoc)
+            {
+                lShift = adaptedGs[pLoc];
+                while (lShift < x.Length)
+                {
+                    i = pat[pLoc].Location - lShift;
+                    if (i < 0 || pat[pLoc].Character != x[i]) break;
+                    ++lShift;
+                    lShift = MatchShift(x, pLoc, lShift, pat);
+                }
+                adaptedGs[pLoc] = lShift;
+            }
+        }
+
+        private static int[] CalculateCharFrequency(char[] x, char[] y, int z)
+        {
+            int i;
+            int[] freq = new int[z];
+            for (i = 0; i < x.Length; i++) freq[x[i]]++;
+            for (i = 0; i < y.Length; i++) freq[y[i]]++;
+            return freq;
+        }
+
+        private static void PreQsBc(char[] x, int[] qsBc)
+        {
+            int i, m = x.Length;
+
+            for (i = 0; i < qsBc.Length; ++i)
+                qsBc[i] = m + 1;
+
+            for (i = 0; i < m; ++i)
+                qsBc[x[i]] = m - i;
+        }
+
+        private static void SetupOptimalSearch()
+        {
+            OrderPattern(_x, _pattern, _frequency);
+            PreQsBc(_x, _qsBc);
+            PreAdaptedGs(_x, _adaptedGs, _pattern);
+        }
+
+        public static void InitOptimalSearch(string pattern, string source)
+        {
+            _x = pattern.ToCharArray();
+            _y = source.ToCharArray();
+            _m = _x.Length;
+            _n = _y.Length;
+            _adaptedGs = new int[_m + 1];
+            _qsBc = new int[65536];
+            _frequency = CalculateCharFrequency(_x, _y, 65536);
+            _pattern = new Pattern[_m];
+        }
+
+        public static Result FindAll()
+        {
+            int i, j;
+            List<int> result = new List<int>();
+            SetupOptimalSearch();
+
+            j = 0;
+            int jOld = 0;
+            while (j <= _n - _m)
+            {
+                i = 0;
+                while (i < _m && _pattern[i].Character == _y[j + _pattern[i].Location]) ++i;
+
+                if (i >= _m)
+                    result.Add(j);
+
+                jOld = j;
+                if (j < _n - _m)
+                    j += Math.Max(_adaptedGs[i], _qsBc[_y[j + _m]]);
+                else
+                    j += _adaptedGs[i];
+            }
+
+            return new Result(jOld, result);
+        }
+
+        public struct Result
+        {
+            public int Comp;
+            public List<int> Indexes;
+
+            public Result(int comp, List<int> indexes)
+            {
+                Comp = comp;
+                Indexes = indexes;
+            }
+        }
+
+        public struct Pattern
+        {
+            public int Location;
+            public char Character;
+        }
+
+    }
+
+}
